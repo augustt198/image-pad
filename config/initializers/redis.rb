250a88d1be25b202611a327eaa461f2850fa0f70
August <augustt198@gmail.com>
2014-06-06 19:42:49 -0400

    Fix redis stuff

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,8 @@
-yml = YAML.load_file(Rails.root.join('config', 'redis.yml'))
+path = Rails.root.join('config', 'redis.yml')
+if File.exists? path
+  yml = YAML.load_file path
+elsif ENV['REDISTOGO_URL']
+  yml = {'url' => ENV['REDISTOGO_URL']}
+end
 yml = {} unless yml.is_a? Hash
 REDIS = Redis.new(yml.symbolize_keys)
