2fc33bbd35a1ad6d66f8c497655bb73db4408909
August <augustt198@gmail.com>
2014-06-06 21:26:17 -0400

    Fix blank text

--- config/initializers/redirect.rb
+++ config/initializers/redirect.rb
@@ -4,6 +4,8 @@ path = Rails.root.join('config', 'redirect.txt')
 if File.exists? path
   REDIRECT_URL = File.open(path).read.gsub("\n", "")
   puts 'using ' + REDIRECT_URL + ' as redirect url'
+elsif ENV['IMG_REDIRECT_URL']
+  REDIRECT_URL = ENV['IMG_REDIRECT_URL']
 else
   # The default redirect goes back to the image
   puts 'redirect.txt file not found - using /img for redirect url'
